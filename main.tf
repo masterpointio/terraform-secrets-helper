71093333993c1f84de832ee0b7717800b1f4d5cb
Jeremy White <jeremy.white@masterpoint.io>
2025-10-16 14:32:35 -0400

    feat(main.tf): internal upstream of ssm support

--- main.tf
+++ main.tf
@@ -23,11 +23,34 @@ locals {
     mapping.name => lookup(local.sops_yamls[mapping.file], mapping.name, null)
   }
 
-  # The final secrets for generic consumption
-  secrets = local.sops_secrets
+  # Filter out our ssm mappings
+  ssm_secret_mapping = [
+    for mapping in var.secret_mapping :
+    mapping if mapping.type == "ssm"
+  ]
+
+  # Collect the ssm paths we need to pull
+  ssm_paths = toset(distinct([
+    for mapping in local.ssm_secret_mapping :
+    mapping.path
+  ]))
+
+  # Create our ssm secret name to value map
+  ssm_secrets = {
+    for mapping in local.ssm_secret_mapping :
+    mapping.name => data.aws_ssm_parameter.ssm_secrets[mapping.path].value
+  }
+
+  # Merge the final ssm secrets + sops secrets for generic consumption in the component.
+  secrets = merge(local.sops_secrets, local.ssm_secrets)
 }
 
 data "sops_file" "sops_secrets" {
   for_each    = local.sops_files
   source_file = "${path.root}/${each.value}"
+}
+
+data "aws_ssm_parameter" "ssm_secrets" {
+  for_each = local.ssm_paths
+  name     = each.value
 }
